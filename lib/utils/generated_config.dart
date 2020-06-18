da2bc1d4bf01a603eb16302ae0b5a47972b61625
Chaobin Wu <chaobinwu89@gmail.com>
2020-06-18 09:50:18 +0800

    Release of 0.1.5

--- lib/utils/generated_config.dart
+++ lib/utils/generated_config.dart
@@ -1,7 +1,7 @@
 /// This file will be updated when ship to pub.dev.
 /// Do not modify manually.
 ///
-/// Date: Tue Jun  9 13:20:14 CST 2020
+/// Date: Thu Jun 18 09:06:51 CST 2020
 /// Author: aven
-const VERSION_NAME = '0.1.4-hotfix1';
+const VERSION_NAME = '0.1.5';
 const RELEASE = 1;
